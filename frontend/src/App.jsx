4f24951503a6aeeb9a34b7bced67c7dd720788fa
Divyanshu Sindhu <divyanshu.sindhu@gmail.com>
2025-08-03 22:05:22 +0530

    - Added react-router-dom for routing capabilities. - Updated App component to include routes for Feed, Messages, ChatBox, Connections, Profile, and CreatePost. - Created new components: ChatBox, Connections, CreatePost, Discover, Feed, Messages, and Profile. - Updated Login component to use className instead of class for JSX compatibility. - Wrapped App component in BrowserRouter for routing context.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
 import Login from './components/Login'
+import { Route, Routes } from 'react-router-dom'
+import Feed from './components/Feed'
+import Messages from './components/Messages'
+import ChatBox from './components/ChatBox'
+import Connections from './components/Connections'
+import Profile from './components/Profile'
+import CreatePost from './components/CreatePost'
 
 const App = () => {
   return (
     <>
-    <Login/>
+    <Routes>
+      <Route path="/" element={<Login />} >
+      <Route index element={<Feed/>} />
+      <Route path='messages' element={<Messages />} />
+      <Route path='messages/:userId' element={<ChatBox />} />
+      <Route path='connections' element={<Connections />} />
+      <Route path='profile' element={<Profile />} />
+      <Route path='profile/profileId' element={<Profile />} />
+      <Route path='create-post' element={<CreatePost />} />
+      </Route>
+    </Routes>
     </>
   )
 }
