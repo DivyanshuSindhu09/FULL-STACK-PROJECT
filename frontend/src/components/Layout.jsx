82ee7b5aec01c19601153b3eba1da3b5521c47d9
Divyanshu Sindhu <divyanshu.sindhu@gmail.com>
2025-08-04 14:52:50 +0530

    Add assets and layout components for improved UI
    - Created assets.js to manage image and icon imports.
    - Added new assets: logo.svg, sample_cover.jpg, sample_profile.jpg, bgImage.png, group_users.png, sponsored_img.png, and favicon.svg.
    - Implemented Sidebar component for navigation.
    - Updated Layout component to include Sidebar and manage sidebar visibility.
    - Removed background color from body in index.css for a cleaner look.

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { SignOutButton, UserProfile, UserButton } from '@clerk/clerk-react'
+import Sidebar from '../templates/Sidebar'
+import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
+  const [sideBarOpen, setSideBarOpen] = useState(true)
   return (
     <>
-    <UserButton />
-    <SignOutButton>LogOUt</SignOutButton>
+    <section className='w-full min-h-screen flex'>
+      <Sidebar/>
+      <div className='flex-1 bg-slate-900'>
+        <Outlet/>
+      </div>
+      {
+        sideBarOpen ?
+        (<i className="ri-close-circle-line absolute top-3 right-3 p-2 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden" onClick={()=>setSideBarOpen(false)}></i>) 
+        : (<i className="ri-menu-2-line absolute top-3 right-3 p-2 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden" onClick={()=>setSideBarOpen(true)}></i>)
+      }
+    </section>
     </>
   )
 }
