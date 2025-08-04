e3b84817295542c4a3ad48e4f6d6e08abd40525e
Divyanshu Sindhu <divyanshu.sindhu@gmail.com>
2025-08-04 12:44:12 +0530

    enhanced Login component design

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -2,15 +2,44 @@ import { SignIn } from "@clerk/clerk-react"
 
 const Login = () => {
   return (
-    <div className='h-screen inset-0 w-full bg-slate-900 fixed flex items-center justify-center overflow-hidden'>
+    <section className='h-screen inset-0 w-full bg-slate-900 fixed flex items-center justify-center overflow-hidden'>
     
         <div className="absolute w-72 top-20 left-10 h-72 blur-3xl opacity-30 animate-pulse transition-all rounded-full  bg-gradient-to-r from-purple-700 via-pink-500 to-red-500 shadow-lg"></div>
-        <div className="absolute w-96 h-96 blur-3xl opacity-30 top-1/3 right-10 animate-pulse transition-all rounded-full  bg-gradient-to-br from-cyan-500 via-blue-600 to-indigo-800 shadow-lg"></div>
+        <div className="absolute w-130 h-130 blur-3xl opacity-30 top-1/3 right-10 animate-pulse transition-all rounded-full  bg-gradient-to-br from-cyan-500 via-blue-600 to-indigo-800 shadow-lg"></div>
         <div className="absolute w-64 h-64 bottom-20 left-1/4 blur-3xl opacity-30 animate-pulse transition-all rounded-full  bg-gradient-to-tr from-[#f12711] to-[#f5af19] shadow-lg"></div>
         <div className="absolute w-80 h-80 bottom-1/3 right-1/3 blur-3xl opacity-30 animate-pulse transition-all rounded-full  bg-gradient-to-r from-[#43cea2] to-[#185a9d]     shadow-lg"></div>
 
-      <SignIn/> 
-    </div>
+      <div className="w-[55%] z-9999 text-center h-full flex flex-col justify-center items-center">
+        <h1 className="text-[7vw] leading-none font-[gilroy] text-[#f4f4f4]">
+          <span className="bg-gradient-to-r font-bold text-[10vw] z-99999999 from-blue-400 via-purple-500 to-cyan-400 bg-clip-text text-transparent animate-pulse"> AXORA </span>
+          <br />
+          Stay Close
+        </h1>
+        <p className="text-[#f4f4f4] text-xl w-[85%] mt-10 font-[absans]">
+          Crafted for meaningful interactions in a world full of noise — Experience a better way to connect.
+        </p>
+      </div>
+      <div className="flex-1  h-full justify-center items-center flex">
+        <SignIn
+        appearance={{
+          elements: {
+            card: {
+              backgroundColor: 'rgba(255, 255, 255, 0.1)',
+              backdropFilter: 'blur(10px)',
+              borderRadius: '20px',
+              boxShadow: '0 4px 30px rgba(0, 0, 0, 0.1)',
+              fontFamily: 'absans',
+              fontSize: '20px',
+            },
+            footer: {
+              display: 'none',
+            },
+          }
+        }}
+        
+      />
+      </div>
+    </section>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
- import { ClerkProvider } from '@clerk/clerk-react'
+import { ClerkProvider } from '@clerk/clerk-react'
+import {shadesOfPurple} from '@clerk/themes'
 
   // Import your Publishable Key
   const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
@@ -13,7 +14,11 @@ import { BrowserRouter } from 'react-router-dom'
   }
 
 createRoot(document.getElementById('root')).render(
-  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+  <ClerkProvider
+  appearance={{
+    baseTheme : [shadesOfPurple]
+  }}
+  publishableKey={PUBLISHABLE_KEY}>
   <BrowserRouter>
   <App/>
   </BrowserRouter>
