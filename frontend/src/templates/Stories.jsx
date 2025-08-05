d7bc31f2ac4159029ef3ab858d3fd6742697a797
Divyanshu Sindhu <divyanshu.sindhu@gmail.com>
2025-08-05 23:16:16 +0530

    changes to be done

--- frontend/src/templates/Stories.jsx
+++ frontend/src/templates/Stories.jsx
@@ -15,12 +15,7 @@ const Stories = () => {
   return (
     <section className='w-screen overflow-x-auto sm:w-[calc(100vw-240px)] px-4 py-2'>
 
-        <div className='flex gap-4 pb-5'>
-            {/* Add Story Card */}
-            <div>
-            </div>
-                {/* stories */}
-        </div>
+      
 
     </section>
   )
