28a05727859a762eb689cc2f3ca1b86c377d68c2
Divyanshu Sindhu <divyanshu.sindhu@gmail.com>
2025-08-05 22:57:47 +0530

    refactor Stories component for improved structure and readability

--- frontend/src/templates/Stories.jsx
+++ frontend/src/templates/Stories.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyStoriesData } from '../../public/assets/assets'
 
 const Stories = () => {
+    const [stories, setStories] = useState([0])
+
+    const fetchStories = async () => {
+        setStories(dummyStoriesData)
+    }
+
+    useEffect(() => {
+        fetchStories()
+    }, [])
+
   return (
-    <div>Stories</div>
+    <section className='w-screen overflow-x-auto sm:w-[calc(100vw-240px)] px-4 py-2'>
+
+        <div className='flex gap-4 pb-5'>
+            {/* Add Story Card */}
+            <div>
+            </div>
+                {/* stories */}
+        </div>
+
+    </section>
   )
 }
 
