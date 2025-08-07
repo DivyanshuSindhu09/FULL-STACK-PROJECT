264e6bb2a38f4f7e42425598fc0d1ca98d4bf876
Divyanshu Sindhu <divyanshu.sindhu@gmail.com>
2025-08-07 11:13:11 +0530

    update user

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,6 @@
+import imagekit from "../config/imagekit.js";
 import {User} from "../models/user.model.js";
+import fs from "fs";
 //! get user data using user id
 
 export const getUserData = async (req, res) => {
@@ -24,6 +26,96 @@ export const getUserData = async (req, res) => {
         return res.status(400).json({
             success : false,
             message : error.message
+        })
+    }
+}
+
+//! update user data
+
+export const updateUserData = async (req, res) => {
+    try {
+        const {userID} = req.auth()
+        const {username, bio,full_name ,location} = req.body
+
+        const tempUser = await User.findById(userID)
+
+        //! if username not provided, use existing username
+        !username && (username = tempUser.username)
+
+        //! 
+        if(tempUser.username !== username) {
+            const user = await User.findOne({username})
+            if(user) {
+                return res.status(400).json({
+                    success: false,
+                    message: "Username already exists"
+                })
+            }
+        }
+
+        const updatedData = {
+            username,
+            bio,
+            full_name,
+            location
+        }
+
+        const profile = req.files.profile_picture && req.files.profile_picture[0]
+        const cover = req.files.cover_picture && req.files.profile_picture[0]
+
+        //! image kit
+
+        if(profile) {
+            const buffer = fs.readFileSync(profile.path)
+            const response = await imagekit.upload({
+                file: buffer,
+                fileName: profile.originalname,
+            })
+
+            const profileUrl = imagekit.url({
+                path: response.filePath,
+                transformation:[
+                    {quality: 'auto'},
+                    {format : 'webp'},
+                    {width:'512'}
+                ]
+            })
+            //! storing url in database
+            updatedData.profile_picture = profileUrl 
+        }
+
+        if(cover) {
+            const buffer = fs.readFileSync(cover.path)
+            const response = await imagekit.upload({
+                file: buffer,
+                fileName: cover.originalname,
+            })
+
+            const coverUrl = imagekit.url({
+                path: response.filePath,
+                transformation:[
+                    {quality: 'auto'},
+                    {format : 'webp'},
+                    {width:'1280'}
+                ]
+            })
+            //! storing url in database
+            updatedData.cover_picture = coverUrl 
+        }
+
+        //! saving in the database
+        const user = await User.findByIdAndUpdate(userID, updatedData, {new: true})
+
+        res.status(200).json({
+            success: true,
+            user,
+            message: "User data updated successfully"
+        })
+    } catch (error) {
+        console.log(error.message)
+        return res.status(400).json({
+            success: false,
+            message: error.message
         })
     }
 }
