20ef853ed86958265adff4e337428301fbb73144
Divyanshu Sindhu <divyanshu.sindhu@gmail.com>
2025-08-07 22:57:28 +0530

    inngest function for sending mails to users for new connection requests

--- backend/inngest/index.js
+++ backend/inngest/index.js
@@ -1,5 +1,6 @@
 import { Inngest } from "inngest";
 import { User } from "../models/user.model.js";
+import { Connection } from "../models/connection.model.js";
 
 // Create Inngest client
 export const inngest = new Inngest({ id: "axora" });
@@ -82,6 +83,28 @@ const syncUserDeletion = inngest.createFunction(
     }
   }
 );
+
+//! function to send reminder for a connection request
+
+const sendNewConnectionReminder = inngest.createFunction(
+  {id: "send-new-connection-request-reminder"},
+  {event : "app/connection-request"},
+  async ({event, step}) => {
+    const {connectionId} = event.data
+
+    await step.run('send-connection-request-mail', async ()=> {
+      const connection = await Connection.findById(connectionId).populate('from_user_id to_user_id')
+      const subject = `New Connection Request`
+      const body = `<div style="font-family: Arial, sans-serif; padding: 20px;">
+  <h2>Hi ${connection.to_user_id.full_name},</h2>
+  <p>You have a new connection request from ${connection.from_user_id.full_name} – @${connection.from_user_id.username}</p>
+  <p>Click <a href="${process.env.FRONTEND_URL}/connections" style="color: #10b981;">here</a> to accept or reject the request</p>
+  <br/>
+  <p>Thanks,<br/>PingUp – Stay Connected</p>
+</div>`
+    })
+  }
+)
 
 // Export Inngest functions
 export const functions = [syncUserCreation, syncUserUpdation, syncUserDeletion];
