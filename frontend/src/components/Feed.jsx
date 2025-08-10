ecf2c9f993ca5905f92e1309a96601ae13ff90ef
Divyanshu Sindhu <divyanshu.sindhu@gmail.com>
2025-08-10 19:26:52 +0530

    story component

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -18,14 +18,14 @@ const Feed = () => {
     <section className='h-full text-white  py-5 xl:pr-5 flex items-start justify-center xl:gap-8'>
 
       {/* stories and post list */}
-      <div className='bg-amber-300 min-h-full w-[70%]'>
+      <div className=' min-h-full w-[70%]'>
         <Stories/>
         Stories Here
         <div className='p-4 space-y-6'>YAHA AeNGI POSTEN BC</div>
       </div>
 
       {/* right side bar */}
-      <div className='w-[25%] bg-amber-600 min-h-full'>
+      <div className='w-[25%]  min-h-full'>
         <div>
           <h2>Sponsered</h2>
         </div>
--- frontend/src/templates/Stories.jsx
+++ frontend/src/templates/Stories.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { dummyStoriesData } from '../../public/assets/assets'
+import moment from "moment"
 
 const Stories = () => {
     const [stories, setStories] = useState([0])
@@ -12,16 +13,59 @@ const Stories = () => {
         fetchStories()
     }, [])
 
-  return (
-    <section className="max-w-full px-4 py-2">
-  <div className="flex space-x-5 overflow-x-auto no-scrollbar py-5 px-3 bg-amber-800">
-    {[...Array(10)].map((_, i) => (
-      <div key={i} className="w-20 h-20 bg-amber-50 flex-shrink-0"></div>
-    ))}
-  </div>
-</section>
+    return (
+        <section className="max-w-full px-4 py-2">
+            <div className="flex gap-3 overflow-x-auto no-scrollbar py-5 px-3">
+                
+                {/* Add Story Card */}
+                <div className='min-w-23 rounded-lg h-30 bg-gradient-to-br from-[#1e293b] to-[#334155] flex flex-col justify-center items-center border-dashed border-2 border-gray-400/40 py-5 px-1 text-sm font-[absans] shadow-lg shadow-purple-800/30 hover:shadow-purple-500/40 transition duration-300'>
+                    <i className="text-xl ri-add-circle-line"></i>
+                    <p>Add Story</p>
+                </div>
 
-  )
+                {/* Story Cards */}
+                {
+                    stories.map((story, index) => (
+                        <div
+                            className='min-w-23 rounded-lg relative h-30 bg-gradient-to-br from-[#1e293b] to-[#334155] shadow-lg shadow-purple-800/30 hover:shadow-purple-500/40 transition duration-300 py-5 px-2 text-sm font-[absans]'
+                            key={index}
+                        >
+                            <img
+                            src={story.user?.profile_picture ? story?.user.profile_picture : null}
+                            className='z-1 absolute size-8 rounded-full top-1 left-1 ring-2 ring-purple-400 ring-offset-2 ring-offset-[#1e293b] shadow'
+                            alt=""
+                            />
+
+                            <p className='z-1 absolute truncate top-15 left-3 text-sm max-w-18'>
+                                {story.content}
+                            </p>
+                            <p className='z-1 absolute bottom-1 right-1 text-xs'>
+                                {moment(story.createdAt).fromNow()}
+                            </p>
+
+                            {
+                                story.media_type !== "text" && (
+                                    <div className='absolute inset-0 z-0 overflow-hidden rounded-lg'>
+                                        {
+                                            story.media_type === "image" ?
+                                                <img
+                                                    className='h-full w-full object-cover hover:scale-110 transition duration-500 opacity-70 hover:opacity-80'
+                                                    src={story.media_url} alt=""
+                                                /> :
+                                                <video
+                                                    className='h-full w-full object-cover hover:scale-110 transition duration-500 opacity-70 hover:opacity-80'
+                                                    src={story.media_url}
+                                                />
+                                        }
+                                    </div>
+                                )
+                            }
+                        </div>
+                    ))
+                }
+            </div>
+        </section>
+    )
 }
 
 export default Stories
