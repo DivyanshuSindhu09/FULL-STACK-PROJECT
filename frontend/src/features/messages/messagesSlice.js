1c7b0809948d410a55049d19dab13810d43ba19f
Divyanshu Sindhu <divyanshu.sindhu@gmail.com>
2025-08-14 16:40:43 +0530

    messageSlice

---
+++ frontend/src/features/messages/messagesSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    value : []
+}
+
+const meessageSlice = createSlice({
+    name : "message",
+    initialState,
+    reducers : {
+
+    }
+})
+
+export default meessageSlice.reducer
